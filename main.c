181f8d4810bec887125bb1ea200192f3436f4d47
HackerCletus <gjsd.cruz98@gmail.com>
2018-11-02 22:31:13 +0000

    Main now parses "config.txt"

--- main.c
+++ main.c
@@ -5,23 +5,19 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <semaphore.h>
-#include <sys/wait.h>
-#include "sim_manager.h"
-#include "central_proc.h"
-#include "log.h"
+#include <sim_manager.h>
 
 
 int main(int argc, char const *argv[]){
 
-  log_it("SIMULATION INITIATED");
-
     int * shared_var;
     int shmid;
     sem_t *mutex;
+    
+    int max_x, max_y;
+    char* productlist[];
 
-    double max_x, max_y;
-    char* productlist;
+    readconfig();
 
 	shmid = shmget(IPC_PRIVATE, __getpagesize(),IPC_CREAT|0700);
 	shared_var = (int*) shmat(shmid, NULL, 0);
@@ -29,13 +25,8 @@ int main(int argc, char const *argv[]){
     pid_t process = fork();
     if (process == 0){
         central_proc(shmid);
-        exit(0);
     } else {
-      wait(NULL);
+        
     }
-    shmctl(shmid, IPC_RMID, NULL);
-
-    log_it("SIMULATION TERMINATED");
-
     return 0;
 }
