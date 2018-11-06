7232503d3b556cc1cc84e0bb1d0a7334cc4c2c3f
HackerCletus <gjsd.cruz98@gmail.com>
2018-11-06 18:54:30 +0000

    Fixed segmentation faults when parsing config.txt

--- main.c
+++ main.c
@@ -31,131 +31,129 @@ typedef struct wnode {
     char* name;
     int chartx, charty;
     wpnode_t* plist_head;
-    struct wnode* next;
 } wnode_t;
 
-// Config list struct
-typedef struct llist {
+// Config variables
     int max_x;
     int max_y;
     pnode_t* product_head;
-    wnode_t* warehouse_head;
-    int* n_of_drones, refill_rate, quantity, time_unit, *n_of_whouses;
-} llist_t;
+    int n_of_drones, refill_rate, quantity, time_unit, n_of_whouses;
 
 int main(int argc, char const *argv[]){
 
 //################# CONFIG PARSING ################################
 // Chunk of code parses info from config.txt to data struct "info" of type llist_t
-    // llist_t* info;
-    // info->product_head = NULL;
-    // info->warehouse_head = NULL;
-    // info->warehouse_head->plist_head = NULL;
-    //
-    //
-    // FILE* conf;
-    // char buffer[255];
-    // const char delim[2] = ", ";
-    // char* token;
-    // conf = fopen("config.txt", "r");
-    // fscanf(conf, "%s ", buffer);
-    // token = strtok(buffer, delim);
-    // info->max_x = atoi(token);
-    // token = strtok(NULL, delim);
-    // info->max_y = atoi(token);
-    //
-    // fscanf(conf, "%s ", buffer);
-    // const char delim2[2] = " ";
-    // token = strtok(buffer, delim2);
-    // while(token != NULL){
-    //     if(info->product_head == NULL){
-    //         pnode_t* phead;
-    //         phead->name = token;
-    //         phead->next = NULL;
-    //
-    //         info->product_head = phead;
-    //     } else {
-    //         pnode_t* aux = (pnode_t*) malloc (sizeof(pnode_t));
-    //         pnode_t* nav = info->product_head;
-    //         aux->name = token;
-    //         aux->next = NULL;
-    //         while(nav->next != NULL){
-    //             nav = nav->next;
-    //         }
-    //         nav->next = aux;
-    //         free(aux);
-    //     }
-    // fscanf(conf, "%i", info->n_of_drones);
-    //
-    // fscanf(conf, "%s ", buffer);
-    // token = strtok(buffer, delim);
-    // info->quantity = atoi(token);
-    // token = strtok(NULL, delim);
-    // info->refill_rate = atoi(token);
-    // token = strtok(NULL, delim);
-    // info->time_unit = atoi(token);
-    //
-    // fscanf(conf, "%i", info->n_of_whouses);
-    //
-    //
-    // int i;
-    // for (i=1; i<*(info->n_of_whouses); i++){
-    //     fscanf(conf, "%s ", buffer);
-    //     token = strtok(buffer, delim2);
-    //     if(info->warehouse_head == NULL){
-    //         wnode_t* whead;
-    //         whead->name = token;
-    //         token = strtok(NULL, delim2);
-    //         token = strtok(NULL, delim);
-    //         whead->chartx = atoi(token);
-    //         token = strtok(NULL, delim2);
-    //         whead->charty = atoi(token);
-    //         token = strtok(NULL, delim2);
-    //         while(token != NULL){
-    //             if (whead->plist_head == NULL){
-    //                 wpnode_t* head;
-    //                 token = strtok(NULL, delim);
-    //                 head->name = token;
-    //                 token = strtok(NULL, delim);
-    //                 head->quantity = atoi(token);
-    //                 head->next = NULL;
-    //                 whead->plist_head = head;
-    //                 free(head);
-    //             }
-    //         }
-    //         whead->next = NULL;
-    //         info->warehouse_head = whead;
-    //     } else {
-    //         wnode_t* aux = (wnode_t*) malloc (sizeof(wnode_t));
-    //         wnode_t* nav = info->warehouse_head;
-    //         aux->name = token;
-    //         token = strtok(NULL, delim2);
-    //         token = strtok(NULL, delim);
-    //         aux->chartx = atoi(token);
-    //         token = strtok(NULL, delim2);
-    //         aux->charty = atoi(token);
-    //         token = strtok(NULL, delim2);
-    //         while(token != NULL){
-    //             if (aux->plist_head == NULL){
-    //                 wpnode_t* head;
-    //                 token = strtok(NULL, delim);
-    //                 head->name = token;
-    //                 token = strtok(NULL, delim);
-    //                 head->quantity = atoi(token);
-    //                 head->next = NULL;
-    //                 aux->plist_head = head;
-    //                 free(head);
-    //             }
-    //         }
-    //         while(nav->next != NULL){
-    //             nav = nav->next;
-    //         }
-    //         nav->next = aux;
-    //         free(aux);
-    //     }
-    // }
-    //
-    // }
+
+    char header[5][256];
+    char *token;
+
+    const char delim[3] = ", ";
+
+    product_head = NULL;
+
+    FILE *conf = fopen("config.txt", "r");
+
+    if (conf == NULL){
+        perror("Config file not found");
+        return 0;
+    }
+
+    for (int i=0; i<5; i++){
+        fgets(header[i], 255, conf);
+    }
+
+
+
+    token = strtok(header[0], delim);
+    max_x = atoi(token);
+    token = strtok(NULL, delim);
+    max_y = atoi(token);
+
+    token = strtok(header[1], delim);
+    int j = 0;
+    while(token != NULL){
+        if(product_head == NULL){
+            product_head = (pnode_t*) malloc(sizeof(pnode_t));
+            product_head->name = (char*) malloc(sizeof(char)*30);
+            product_head->next = NULL;
+            strcpy(product_head->name, token);
+        } else {
+            pnode_t* nav = product_head;
+            pnode_t* aux = (pnode_t*) malloc(sizeof(pnode_t));
+            aux->name = (char*) malloc(sizeof(char)*30);
+            strcpy(aux->name, token);
+            aux->next = NULL;
+
+            while (nav->next != NULL){
+                nav = nav->next;
+            }
+            nav->next = aux;
+        }
+        token = strtok(NULL, delim);
+        j++;
+    }
+
+    n_of_drones = atoi(header[2]);
+    token = strtok(header[3], delim);
+    refill_rate = atoi(token);
+    token = strtok(NULL, delim);
+    quantity = atoi(token);
+    token = strtok(NULL, delim);
+    time_unit = atoi(token);
+
+    token = strtok(header[4], delim);
+    n_of_whouses = atoi(token);
+
+    char whouse_data [n_of_whouses][255];
+    wnode_t whouse[n_of_whouses];
+
+    for(int i = 0; i<n_of_whouses; i++){
+        fgets(whouse_data[i], 255, conf);
+    }
+
+    for(int i = 0; i<n_of_whouses; i++){
+        token = strtok(whouse_data[i], " ");
+        whouse[i].name = token;
+        token = strtok(NULL, " ");
+        token = strtok(NULL, " ");
+        whouse[i].chartx = atoi(token);
+        token = strtok(NULL, delim);
+        whouse[i].charty = atoi(token);
+        whouse[i].plist_head = NULL;
+        token = strtok(NULL, " ");
+        token = strtok(NULL, delim);
+        while(token != NULL){
+            if(whouse[i].plist_head == NULL){
+                whouse[i].plist_head = (wpnode_t*) malloc(sizeof(wpnode_t));
+                (whouse[i].plist_head)->name = token;
+                token = strtok(NULL, delim);
+                if(token == NULL){
+                    perror("Config file error: Warehouse product description mismatch\n");
+                    return 0;
+                }
+                (whouse[i].plist_head)->quantity = atoi(token);
+                (whouse[i].plist_head)->next = NULL;
+            } else {
+                wpnode_t* navg = whouse[i].plist_head;
+                wpnode_t* auxw = (wpnode_t*) malloc (sizeof(wpnode_t));
+                auxw->name = token;
+                token = strtok(NULL, delim);
+                if(token == NULL){
+                    perror("Config file error: Warehouse product description mismatch\n");
+                    return 0;
+                }
+                auxw->quantity = atoi(token);
+                auxw->next = NULL;
+
+                while(navg->next != NULL){
+                    navg = navg->next;
+                }
+                navg->next = auxw;
+            }
+            token = strtok(NULL, delim);
+        }
+    }
+
 
 //####################################################################
 
